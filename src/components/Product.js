811fdb5877ae850193bc1c616dc151832a0bc003
YasinCelikSoftware <yasin.celik4@outlook.com>
2023-08-10 01:05:23 +0300

    Add to cart function added to product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import '../css/Product.css';
 import prime from '../assets/images/prime-icon.png';
+import {useDispatch, useSelector} from 'react-redux';
+import * as cartActions from '../redux/actions/cartActions';
 
 function Product (props) {
+  const dispatch = useDispatch ();
+
+  const addToCart = product => {
+    dispatch (
+      cartActions.addToCart ({
+        id: props.id,
+        title: props.title,
+        image: props.image,
+        rating: props.rating,
+        alt: props.alt,
+        price: props.price,
+      })
+    );
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -11,14 +28,16 @@ function Product (props) {
           <small>₺</small><strong>{props.price}</strong>
         </p>
         <div className="product__rating">
-          {Array (props.rating).fill ().map ((_, i) => <p>⭐</p>)}
+          {Array (props.rating).fill ().map ((_, i) => <p key={i}>⭐</p>)}
         </div>
       </div>
       <img src={props.image} className="product__image" alt={props.alt} />
       {props.prime
         ? <img className="product__primeIcon" src={prime} alt="prime" />
         : ''}
-      <button className="product__button">Sepete Ekle</button>
+      <button onClick={addToCart} className="product__button">
+        Sepete Ekle
+      </button>
     </div>
   );
 }
