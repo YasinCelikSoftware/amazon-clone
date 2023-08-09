d0c3940d9568e73c1dae9ee4c137665a544e07f3
YasinCelikSoftware <yasin.celik4@outlook.com>
2023-08-09 16:47:45 +0300

    Some route corrections

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import NotFound from './NotFound';
+import Checkout from './components/Checkout';
 import Header from './components/Header';
 import Home from './components/Home';
 import {Routes, Route} from 'react-router-dom';
@@ -7,10 +8,11 @@ import {Routes, Route} from 'react-router-dom';
 function App () {
   return (
     <div className="app">
+      <Header />
       <Routes>
-        <Route path="/" element={<><Header />
-          <Home /></>} />
-        <Route path='*' element={<NotFound></NotFound>}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/sepet" element={<Checkout />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
