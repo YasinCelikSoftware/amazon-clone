697e9a6d8606ac8be0e32b10a899b6d04d270140
YasinCelikSoftware <yasin.celik4@outlook.com>
2023-08-09 16:01:50 +0300

    Routes and Route added from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import NotFound from './NotFound';
 import Header from './components/Header';
 import Home from './components/Home';
+import {Routes, Route} from 'react-router-dom';
 
 function App () {
   return (
     <div className="app">
-      <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<><Header />
+          <Home /></>} />
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
