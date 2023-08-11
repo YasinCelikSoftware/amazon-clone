b4afd2fe2c478cff0196ee6f272753c5a186416a
YasinCelikSoftware <yasin.celik4@outlook.com>
2023-08-11 21:18:58 +0300

    SignIn authentication

--- src/App.js
+++ src/App.js
@@ -5,8 +5,25 @@ import Checkout from './components/Checkout';
 import Home from './components/Home';
 import {Routes, Route} from 'react-router-dom';
 import Login from './components/Login';
+import {useEffect} from 'react';
+import {auth} from './firebase';
+import {useDispatch} from 'react-redux';
+import * as userActions from './redux/actions/userActions';
 
 function App () {
+  const dispatch = useDispatch ();
+
+  useEffect (() => {
+    auth.onAuthStateChanged (authUser => {
+      console.log ('THE USER IS -> ', authUser);
+      if (authUser) {
+        dispatch (userActions.setUser (authUser));
+      } else {
+        dispatch (userActions.setUser (null));
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
       <Routes>
