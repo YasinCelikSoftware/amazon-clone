fadd58faa61760b87c7619580c674bd3dc586a49
YasinCelikSoftware <yasin.celik4@outlook.com>
2023-08-12 14:44:54 +0300

    Reducer correction

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -4,7 +4,8 @@ import initialState from './initialState';
 export default function userReducer (state = initialState.user, action) {
   switch (action.type) {
     case actionTypes.SET_USER:
-      return {...state, ...action.payload};
+      return action.payload !== null ? {...action.payload} : null;
+
     default:
       return state;
   }
